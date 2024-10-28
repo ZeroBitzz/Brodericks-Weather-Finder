08b7f22e968691b597eecb503ff9fc6e011caa55
Broderick <zerothesu@gmail.com>
2024-10-27 18:11:45 -0600

    migrated icons and weatherman files

---
+++ weatherman/WeathermanComponent.js
@@ -0,0 +1,23 @@
+import { useRive, useStateMachineInput} from 'rive-react'
+import weatherman from './weatherman.riv'
+
+function WeathermanComponent(){
+    console.log('buffer')
+    const {rive, RiveComponent} = useRive({
+      buffer: weatherman,
+      src: weatherman,
+      stateMachines: 'statemachine',
+      autoplay: true
+    })
+  
+    const weathermanStateSetter = useStateMachineInput(
+      rive,
+      "statemachine",
+      "animation",
+      0
+    )
+      
+    return (<RiveComponent className='weatherman'/>)
+}
+
+export default WeathermanComponent
